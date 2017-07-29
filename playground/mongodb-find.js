4c0694056773778f3e97ab97b8e8dc877faab0ef
Mateusz Nowak <mateusz.nowak.199120@gmail.com>
2017-07-29 11:47:24 +0200

    init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,35 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    console.log('Unable to connect to mongodb server');
+  }
+  console.log('Connected to mongodb server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('597c526e5e62f0478f18d05e')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({name: 'Andrew'}).count().then((count) => {
+    console.log(`Todos count: ${count}`);
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+
+
+
+  //db.close();
+
+});
