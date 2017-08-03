250c2296dc6872678dd8ea9e8b7d78b4bce564fa
Mateusz Nowak <mateusz.nowak.199120@gmail.com>
2017-08-03 17:08:03 +0200

    Add queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,46 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// var id = '5981f48d7d4bfc01c8c97abc';
+//
+// if (!ObjectID.isValid(id)) {
+//   console.log('id not valid');
+// }
+//
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+//
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo by id', todo);
+// }).catch((e) => console.log(e));
+
+var id = '597c94223f404a1714c8f992';
+
+if (!ObjectID.isValid(id)) {
+  console.log('id is not valid');
+}
+
+User.findById(id).then((user) => {
+  if (!user) {
+    return console.log('Unable to find user');
+  }
+
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e);
+});
